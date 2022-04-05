103077feb80996b7992f8debfc5e0ec0cf9e327c
VictorPorlan <victorporlaninformatica@gmail.com>
2022-04-05 15:02:00 +0200

    feat: hover fav y mejoras nombre de variables

--- src/gateways/gatewayPack.js
+++ src/gateways/gatewayPack.js
@@ -1,81 +1,79 @@
 async function getAllPacks() {
-  let result;
-  await fetch("http://localhost:3001/packs/allPacks")
-    .then((res) => res.json())
-    .then((data) => {
-      result = data;
-    });
-  return result;
+    let result;
+    await fetch("http://localhost:3001/packs/allPacks")
+        .then((res) => res.json())
+        .then((data) => {
+            result = data;
+        });
+    return result;
 }
 
 async function getOnePack(nombre) {
-  let result;
-  await fetch(`http://localhost:3001/packs/findOne/${nombre}`)
-    .then((res) => res.json())
-    .then((data) => {
-      result = data;
-    });
-  return result;
+    let result;
+    await fetch(`http://localhost:3001/packs/findOne/${nombre}`)
+        .then((res) => res.json())
+        .then((data) => {
+            result = data;
+        });
+    return result;
 }
 
 async function buyPack(nombre) {
-  await fetch(`http://localhost:3001/packs/buy/${nombre}`).then((x) => reload())
-  reload();
+    await fetch(`http://localhost:3001/packs/buy/${nombre}`).then((_x) => reload())
+    reload();
 }
 
 async function openPack(nombre) {
-  await fetch(`http://localhost:3001/packs/open/${nombre}`).then((x) => reload())
-  reload();
+    await fetch(`http://localhost:3001/packs/open/${nombre}`).then((_x) => reload())
+    reload();
 }
 
 async function useAllItems(nombre) {
-  await fetch(`http://localhost:3001/packs/useItems/${nombre}`);
-  reloadDetails(nombre);
+    await fetch(`http://localhost:3001/packs/useItems/${nombre}`);
+    reloadDetails(nombre);
 }
 
 async function useItem(nombre, index) {
-  await fetch(`http://localhost:3001/packs/useItem/${nombre}/${index}`);
-  reloadDetails(nombre);
+    await fetch(`http://localhost:3001/packs/useItem/${nombre}/${index}`);
+    reloadDetails(nombre);
 }
 
 async function deletePack(nombre) {
-  await fetch(`http://localhost:3001/packs/delete/${nombre}`, {
-    method: "DELETE",
-  });
-  reload();
+    await fetch(`http://localhost:3001/packs/delete/${nombre}`, {
+        method: "DELETE",
+    });
+    reload();
 }
 
 async function createPack(json) {
-  await fetch(`http://localhost:3001/packs/create`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(json),
-  });
-  reload();
+    await fetch(`http://localhost:3001/packs/create`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+    });
+    reload();
 }
 
-async function packAddFavourite(nombre){
-  let tempStorage = JSON.parse(localStorage.getItem('fav'))
-  if(tempStorage !== null){
-    localStorage.setItem('fav', JSON.stringify([nombre, ...tempStorage]))
-    reload()
-  }
-  else{
-    localStorage.setItem('fav', JSON.stringify([nombre]))
-    reload()
-  }
+async function packAddFavourite(nombre) {
+    let tempStorage = JSON.parse(localStorage.getItem('fav'))
+    if (tempStorage !== null) {
+        localStorage.setItem('fav', JSON.stringify([nombre, ...tempStorage]))
+        reload()
+    } else {
+        localStorage.setItem('fav', JSON.stringify([nombre]))
+        reload()
+    }
 }
 
-async function packRemoveFavourite(nombre){
-  let tempStorage = JSON.parse(localStorage.getItem('fav'))
-  if(tempStorage !== null){
-    let result = tempStorage.filter((x) => x !== nombre)
-    localStorage.setItem('fav', JSON.stringify(result))
-    reload()
-  }
-  else{
-    return
-  }
+async function packRemoveFavourite(nombre) {
+    let tempStorage = JSON.parse(localStorage.getItem('fav'))
+    if (tempStorage !== null) {
+        let result = tempStorage.filter((x) => x !== nombre)
+        localStorage.setItem('fav', JSON.stringify(result))
+        reload()
+    } else {
+        return
+    }
 }
